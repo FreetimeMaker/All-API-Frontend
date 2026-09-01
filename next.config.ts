2870837855b233007c9ab35b376528fdbf1128af
Jamie Achatz <jamieachatz@gmail.com>
2026-09-01 12:26:55 +0200

    Fix Certificate Issue (Hopefully)

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,37 @@ const nextConfig: NextConfig = {
   compress: true,
   poweredByHeader: false,
   assetPrefix: "/",
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload"
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'dashboard.free-time.me',
+          },
+        ],
+        destination: 'https://dashboard.free-time.me/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
